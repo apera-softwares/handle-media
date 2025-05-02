312cdfc7ec480232c8c21ed99247c6626740bf0e
pratikSondauleApera <pratik@aperasoftwares.com>
2025-05-02 15:58:12 +0530

    added swagger for api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Auction API's")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config)
+
+  SwaggerModule.setup('docs', app, document)
 
   app.enableCors()
 
